ea45834d4769c41a8da67e687ed10d2b742c9b10
axelle <kadance9720@gmail.com>
2021-04-08 11:19:52 +0200

    Configuration route Accueil

--- src/Controller/SiteAnnoncesImmoController.php
+++ src/Controller/SiteAnnoncesImmoController.php
@@ -4,15 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class SiteAnnoncesImmoController extends AbstractController
 {
-    /**
-     * Page d'accueil
-     * 
-     * @Route("/")
-     */
     public function index(): Response
     {
         //Renvoi de la vue grace à la méthode render()
