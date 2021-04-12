766699c40e793e884766cbdfc4fb508ef3b7acb5
axelle <kadance9720@gmail.com>
2021-04-12 10:10:09 +0200

    Commencement de la mise en forme des pages

--- src/Controller/SiteAnnoncesImmoController.php
+++ src/Controller/SiteAnnoncesImmoController.php
@@ -12,4 +12,26 @@ class SiteAnnoncesImmoController extends AbstractController
         //Renvoi de la vue grace à la méthode render()
         return $this->render('site_annonces_immo/index.html.twig');
     }
+
+    public function add(): Response
+    {
+        return $this->render('site_annonces_immo/propertie_add.html.twig');
+    }
+
+    public function show($url): Response
+    {
+        return $this->render('site_annonces_immo/propertie_show.html.twig', [
+            'slug' => $url
+        ]);
+    }
+
+    public function edit(): Response
+    {
+        return $this->render('site_annonces_immo/propertie_edit.html.twig');
+    }
+
+    public function remove(): Response
+    {
+        return $this->render('site_annonces_immo/propertie_add.html.twig');
+    }
 }
