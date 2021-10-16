ce5d52eda67d188074aedcaefc9f6ff352ca5878
axelle <kadance9720@gmail.com>
2021-10-16 14:19:24 +0200

    Drop db, ajout addroles dans user entity à nouveau

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -99,6 +99,18 @@ class User implements UserInterface
     /**
      * @see UserInterface
      */
+    public function addRoles(string $roles): self
+    {
+        if (!in_array($roles, $this->roles)) {
+            $this->roles[] = $roles;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
     public function getRoles(): array
     {
         $roles = $this->roles;
